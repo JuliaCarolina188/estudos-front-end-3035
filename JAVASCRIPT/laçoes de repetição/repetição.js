0035771efac9ce5529ca6c2be83fb907d0ec758d
JuliaCarolina188 <julicarolyna@gmail.com>
2025-10-30 09:09:13 -0300

    repetição

---
+++ JAVASCRIPT/laçoes de repetição/repetição.js
@@ -0,0 +1,20 @@
+let i = 0
+while (i <= 10) {
+    console.log(i)
+    i++
+}
+i = 0
+
+function tabuada (num) {
+    let j = 0
+    while (j <= 10) {
+    console.log(`${num} x ${j} = ${num*j}`)
+    j++
+    }
+}
+
+tabuada(5)
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}

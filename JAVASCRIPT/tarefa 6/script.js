dc74a9cc07e127c069b958b52a9ba3ed43713b17
JuliaCarolina188 <julicarolyna@gmail.com>
2025-10-30 14:35:49 -0300

    Tentativa da tarefa 6

---
+++ JAVASCRIPT/tarefa 6/script.js
@@ -0,0 +1,12 @@
+//Crie uma lista de tarefas usando objetos JavaScript. Cada tarefa deve ser representada como um objeto contendo informações como o nome da tarefa, a data de criação e o status (concluída ou não). Você também deve criar funções para adicionar tarefas à lista, marcar tarefas como concluídas e listar as tarefas pendentes.
+
+let continuar = false
+let listaDeTarefas = {}
+
+
+while (continuar == false) {
+    console.log('\nLista de tarefas atual:\n')
+    console.log(listaDeTarefas)
+
+    
+}
